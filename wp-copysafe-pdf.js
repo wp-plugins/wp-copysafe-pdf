ba40045f8b7bd44a64a404d7f6183202bad3325d
ArtistScope <ArtistScope@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-12 22:00:00 +0000

    Fixed JS error
    git-svn-id: https://plugins.svn.wordpress.org/wp-copysafe-pdf/trunk@803394 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-copysafe-pdf.js
+++ wp-copysafe-pdf.js
@@ -45,7 +45,7 @@ var m_bWinxp = ((m_szAgent.indexOf("windows nt 5.1")!=-1) || (m_szAgent.indexOf(
 var m_bWin2k3 = (m_szAgent.indexOf("windows nt 5.2")!=-1);	
 var m_bVista = (m_szAgent.indexOf("windows nt 6.0")!=-1);
 var m_bWindows7 = (m_szAgent.indexOf("windows nt 6.1")!=-1);
-var m_bWindows8 = ((m_szAgent.indexOf("windows nt 6.2")!=-1 ||  (m_szAgent.indexOf("windows nt 6.3")!=-1));
+var m_bWindows8 = ((m_szAgent.indexOf("windows nt 6.2")!=-1) ||  (m_szAgent.indexOf("windows nt 6.3")!=-1));
 var m_bWindows = (((m_bWin2k) || (m_bWinxp) || (m_bWin2k3) || (m_bVista) || (m_bWindows7) || (m_bWindows8)) && ((m_bWin32) || (m_bWin64)));
 
 var m_bOpera = ((m_szAgent.indexOf("opera")!=-1) && !!(window.opera && window.opera.version) && (m_bpOpera));
